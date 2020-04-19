6d24e3cf03c11bf7aa235cae2e988598ef975d14
eyllanesc <e.yllanescucho@gmail.com>
2020-04-19 12:17:40 -0500

    add .setAllowedOrigins(*), afterConnectionClosed() and use sessions instead session

--- src/main/java/com/devglan/config/SocketHandler.java
+++ src/main/java/com/devglan/config/SocketHandler.java
@@ -1,10 +1,13 @@
 package com.devglan.config;
 
 import com.google.gson.Gson;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.CloseStatus;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -17,11 +20,18 @@ public class SocketHandler extends TextWebSocketHandler {
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
 		Map<String, String> value = new Gson().fromJson(message.getPayload(), Map.class);
-		session.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+		for (WebSocketSession s : sessions){
+			s.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+		}
 	}
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		sessions.add(session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception{
+		sessions.remove(session);
 	}
 }
