a5d6677c4e7fab653c9b86c52c4bc7b5c4793061
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-04-19 15:23:15 -0300

    SocketHandler

--- src/main/java/com/devglan/config/SocketHandler.java
+++ src/main/java/com/devglan/config/SocketHandler.java
@@ -17,11 +17,18 @@ public class SocketHandler extends TextWebSocketHandler {
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException {
 		Map<String, String> value = new Gson().fromJson(message.getPayload(), Map.class);
-		session.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+		for (WebSocketSession s : sessions) {
+			session.sendMessage(new TextMessage("Hello " + value.get("name") + " !"));
+		}
 	}
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		sessions.add(session);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception{
+		sessions.remove(session);
 	}
 }
